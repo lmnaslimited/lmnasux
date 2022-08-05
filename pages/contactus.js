de843928c621e3e0850b0f52ce637029c844e99e
srinath1204 <srinathmuniyasamy1204@gmail.com>
2022-08-05 23:06:54 +0530

    contactuslintcorrection

--- pages/contactus.js
+++ pages/contactus.js
@@ -47,28 +47,28 @@ function MyApp() {
                        </div>
                        </div>
                        </div>
-                       <section class="text-gray-600 body-font relative bg-white">
-  <div class="container py-12 lg:-ml-32 flex flex-col lg:flex-row bg-white">
-    <div class="lg:w-3/4 md:w-full bg-white rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                       <section className="text-gray-600 body-font relative bg-white">
+  <div className="container py-12 lg:-ml-32 flex flex-col lg:flex-row bg-white">
+    <div className="lg:w-3/4 md:w-full bg-white rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
      <Image src="/contactus/working-women6123x4082.png" width="6000" height="4000" alt="" />
     </div>
-    <div class="lg:w-1/2 md:w-full bg-blue-50 flex flex-col md:ml-auto w-full md:py-8 md:px-12 mt-8 md:mt-0">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
-      <div class="relative mb-4">
-        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-        <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+    <div className="lg:w-1/2 md:w-full bg-blue-50 flex flex-col md:ml-auto w-full md:py-8 md:px-12 mt-8 md:mt-0">
+      <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+      <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+      <div className="relative mb-4">
+        <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+        <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
       </div>
-      <div class="relative mb-4">
-        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+      <div className="relative mb-4">
+        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
       </div>
-      <div class="relative mb-4">
-        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      <div className="relative mb-4">
+        <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+        <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
       </div>
-      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+      <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+      <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
     </div>
   </div>
 </section>
--- pages/manufacturing/whylmnas.js
+++ pages/manufacturing/whylmnas.js
@@ -2,35 +2,35 @@ import Image from "next/image"
 export default function Whylmnas(Whylmnas){
 
     return(
-       <section class="pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] overflow-hidden">
-   <div class="container">
-      <div class="flex flex-wrap justify-between items-center -mx-4">
-         <div class="w-full lg:w-6/12 px-4">
-            <div class="flex items-center -mx-3 sm:-mx-4">
-               <div class="w-full xl:w-1/2 px-3 sm:px-4">
-                  <div class="py-3 sm:py-4">
-                     <img
+       <section className="pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] overflow-hidden">
+   <div className="container">
+      <div className="flex flex-wrap justify-between items-center -mx-4">
+         <div className="w-full lg:w-6/12 px-4">
+            <div className="flex items-center -mx-3 sm:-mx-4">
+               <div className="w-full xl:w-1/2 px-3 sm:px-4">
+                  <div className="py-3 sm:py-4">
+                     <Image width="120" height="120" 
                         src="https://cdn.tailgrids.com/1.0/assets/images/services/image-1.jpg"
                         alt=""
-                        class="rounded-2xl w-full"
+                        className="rounded-2xl w-full"
                         />
                   </div>
-                  <div class="py-3 sm:py-4">
-                     <img
+                  <div className="py-3 sm:py-4">
+                     <Image width="120" height="120" 
                         src="https://cdn.tailgrids.com/1.0/assets/images/services/image-2.jpg"
                         alt=""
-                        class="rounded-2xl w-full"
+                        className="rounded-2xl w-full"
                         />
                   </div>
                </div>
-               <div class="w-full xl:w-1/2 px-3 sm:px-4">
-                  <div class="my-4 relative z-10">
-                     <img
+               <div className="w-full xl:w-1/2 px-3 sm:px-4">
+                  <div className="my-4 relative z-10">
+                     <Image width="120" height="120" 
                         src="https://cdn.tailgrids.com/1.0/assets/images/services/image-3.jpg"
                         alt=""
-                        class="rounded-2xl w-full"
+                        className="rounded-2xl w-full"
                         />
-                     <span class="absolute -right-7 -bottom-7 z-[-1]">
+                     <span className="absolute -right-7 -bottom-7 z-[-1]">
                         <svg
                            width="134"
                            height="106"
@@ -604,27 +604,27 @@ export default function Whylmnas(Whylmnas){
                </div>
             </div>
          </div>
-         <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
-            <div class="mt-10 lg:mt-0">
-               <span class="font-semibold text-lg text-primary mb-2 block">
+         <div className="w-full lg:w-1/2 xl:w-5/12 px-4">
+            <div className="mt-10 lg:mt-0">
+               <span className="font-semibold text-lg text-primary mb-2 block">
                Why Choose Us
                </span>
-               <h2 class="font-bold text-3xl sm:text-4xl text-dark mb-8">
+               <h2 className="font-bold text-3xl sm:text-4xl text-dark mb-8">
                   Make your customers happy by giving services.
                </h2>
-               <p class="text-base text-body-color mb-8">
+               <p className="text-base text-body-color mb-8">
                   It is a long established fact that a reader will be distracted
                   by the readable content of a page when looking at its layout.
                   The point of using Lorem Ipsum is that it has a more-or-less.
                </p>
-               <p class="text-base text-body-color mb-12">
+               <p className="text-base text-body-color mb-12">
                   A domain name is one of the first steps to establishing your
                   brand. Secure a consistent brand image with a domain name that
                   matches your business.
                </p>
                <a
                   href="javascript:void(0)"
-                  class="
+                  className="
                   py-4
                   px-10
                   lg:px-8
